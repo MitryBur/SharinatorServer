0ecd7c31aa0ca32c7196b4681908311b59bda4ca
Mitry Burmistrov <mitekxxx@gmail.com>
2013-07-11 13:34:58 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SharinatorServer::Application.config.secret_key_base = 'a8d66157828ee8dc634bcaca62c3ae0c59bdd55c803ff438cfe6f52222b6f4c46fab41e89b3bd976cf69592e686417b43c279f69582b5c91ab572ff95f1797c3'
