7ba59ea6e5aa5cab8060eb486213ea8ce0f15f2b
Mitry Burmistrov <mitekxxx@gmail.com>
2013-07-14 17:30:48 +0400

    Request friends list from vk

--- app/controllers/vk_controller.rb
+++ app/controllers/vk_controller.rb
@@ -1,5 +1,18 @@
+require 'net/http'
+require 'net/https'
+
 class VkController < ApplicationController
   def authenticate
-    @code = params[:code]
+    token = params[:access_token]
+    uid = params[:user_id]
+    if !token.empty?
+      #User.create :user_id=> 111, :social_id=>222;
+      @url = URI.parse "https://api.vk.com/method/friends.get?uid=#{uid}&access_token=#{token}"
+      http = Net::HTTP.new(@url.host, @url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      res = http.get(@url.to_s)
+      @resBody = res.body
+    end
   end
 end
