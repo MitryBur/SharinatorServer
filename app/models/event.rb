b65d4c1ecd1ab82c62c4edabd46372e24fbd698b
Mitry Burmistrov <mitekxxx@gmail.com>
2013-07-18 13:45:03 +0400

    Migration for Postgres

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
 		has_many :attendances
 		has_many :users, through: :attendances
+
+		def add_attendance id
+
+		end
 end
--- db/migrate/20130718080034_socialid_to_integer.rb
+++ db/migrate/20130718080034_socialid_to_integer.rb
@@ -1,5 +1,12 @@
 class SocialidToInteger < ActiveRecord::Migration
-  def change
+		def up
+				connection.execute(%q{
+            alter table socials
+            alter column social_id
+            type integer using cast(number as integer)
+        })
+		end
+		def change
 		  change_column :socials, :social_id, :integer
 		  change_column :users, :social_id, :integer
   end
