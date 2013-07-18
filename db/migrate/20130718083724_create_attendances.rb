7437b6208a6b27df06c16ce8078d90ff83ffb254
Mitry Burmistrov <mitekxxx@gmail.com>
2013-07-18 13:58:45 +0400

    Migration to Postgres 4

--- db/migrate/20130718083724_create_attendances.rb
+++ db/migrate/20130718083724_create_attendances.rb
@@ -1,6 +1,5 @@
 class CreateAttendances < ActiveRecord::Migration
   def change
-		  drop_table :attendances
 		  create_table :attendances do |t|
       t.references :user, index: true
       t.references :event, index: true
