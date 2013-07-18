57d82aaad4d6f617f0e64063df91a62458bb3bd4
Mitry Burmistrov <mitekxxx@gmail.com>
2013-07-18 13:50:09 +0400

    Migration to Postgres 2

--- db/migrate/20130718080034_socialid_to_integer.rb
+++ db/migrate/20130718080034_socialid_to_integer.rb
@@ -5,9 +5,10 @@ class SocialidToInteger < ActiveRecord::Migration
             alter column social_id
             type integer using cast(number as integer)
         })
+				connection.execute(%q{
+            alter table users
+            alter column social_id
+            type integer using cast(number as integer)
+        })
 		end
-		def change
-		  change_column :socials, :social_id, :integer
-		  change_column :users, :social_id, :integer
-  end
 end
