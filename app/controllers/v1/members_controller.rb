d0b234bc694481615f626460969568dbac55e3f8
Mitry Burmistrov <mitekxxx@gmail.com>
2013-07-24 00:27:43 +0400

    Create member tiny refactor

--- app/controllers/v1/members_controller.rb
+++ app/controllers/v1/members_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class V1::MembersController < ApplicationController
 		before_filter :restrict_access
-		before_action :set_member, only: [:show, :edit, :update, :destroy]
+		before_action :set_member, only: [:show, :update, :destroy]
     respond_to :json
   # GET /members
   # GET /members.json
@@ -23,17 +23,7 @@ class V1::MembersController < ApplicationController
     vk_id = member_params[:user_id]
     user = User.first(include: :social, conditions: {socials: {vk_id: vk_id}})
     unless user
-      fields = "fields=first_name,last_name,sex"
-      token = member_params[:access_token]
-      res = VkAPI.request token: token, method: 'users.get', vk_params:"uids=#{vk_id}&#{fields}"
-      resBody = res.body
-      jsonRes = JSON.parse resBody
-      if jsonRes['error']
-        render json: jsonRes
-        return
-      end
-      user_data = jsonRes['response'][0]
-      user = create_user vk_id: vk_id, name: user_data['first_name'], surname: user_data['last_name']
+      user = create_user vk_id: vk_id
     end
     @member.user = user
     if @member.save
@@ -78,9 +68,38 @@ class V1::MembersController < ApplicationController
 				end
     end
 
+    #not yet implemented
+    def create_multiple_members
+      vk_ids = JSON.parse member_params[:user_id]
+      vk_ids.each do |vk_id|
+        @member = Member.new
+        @member.event_id = member_params[:event_id]
+        user = User.first(include: :social, conditions: {socials: {vk_id: vk_id}})
+        unless user
+          user = create_user vk_id: vk_id
+        end
+        @member.user = user
+        if @member.save
+          render action: 'show', status: :created, location: [:v1, @member]
+        else
+          render json: @member.errors, status: :unprocessable_entity
+        end
+      end
+    end
+
     def create_user params
+      fields = 'fields=first_name,last_name,sex'
+      token = member_params[:access_token]
+      res = VkAPI.request token: token, method: 'users.get', vk_params:"uids=#{params[:vk_id]}&#{fields}"
+      resBody = res.body
+      jsonRes = JSON.parse resBody
+      if jsonRes['error']
+        render json: jsonRes
+        return
+      end
+      user_data = jsonRes['response'][0]
       @user = User.create
-      @social = Social.create vk_id: params[:vk_id], name: params[:name], surname: params[:surname], vk_token: nil
+      @social = Social.create vk_id: params[:vk_id], name: user_data['name'], surname: user_data['surname']
       @user.social = @social
       @user
     end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
-  validates_uniqueness_of :user_id, :scope => [:event_id]
+  validates_uniqueness_of :user_id, :scope => [:event_id], message: 'Already exists'
 
 end
