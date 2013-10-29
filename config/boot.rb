0d54e1166f7edce2bafb63797992cf57a1f1bce7
Yaroslav L <lepenkiny@gmail.com>
2013-10-30 03:53:26 +0400

    fixtures fixed, test for expense validation added

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 # Set up gems listed in the Gemfile.
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
+
+  test "Expense is not valid withot event_id" do
+    e = expenses(:no_event_id)
+    assert !e.valid?
+  end
+
+  test "Expense is valid if event_id exists" do
+    e = expenses(:with_event_id)
+    assert e.valid?
+  end
+
   # test "the truth" do
   #   assert true
   # end
