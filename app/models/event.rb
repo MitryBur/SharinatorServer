d17477970c88d8854bb0dc1f2c067ccca0c0956b
Mitry <mitekxxx@gmail.com>
2014-05-11 13:10:53 +0400

    Fixed event creation Unprocessable entity error

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ActiveRecord::Base
 
   accepts_nested_attributes_for :users
 
-  validates_presence_of :title, :owner
+  validates_presence_of :title, :owner_id
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'Must be a URL for GIF, JPG or PNG image'
--- app/views/v1/users/index.json.jbuilder
+++ app/views/v1/users/index.json.jbuilder
@@ -1,6 +1,8 @@
 json.array!(@users) do |user|
   json.extract! user, :id
+  if user.social_profile
   json.social_profile do |json|
     json.(user.social_profile, :name, :surname, :vk_access_token)
+  end
   end
 end
